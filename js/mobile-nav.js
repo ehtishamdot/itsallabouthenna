648c12be4f6a20a16d2b64659104e62fd718c027
Ehtisham Ul Haq <ahtionpc1@gmail.com>
2021-07-05 18:36:41 +0500

    btn-setting

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,12 +1,12 @@
-const navSlide = () => {
-    const burger = document.querySelector('.ahti-nav-burger');
-    const nav = document.querySelector('.ahti-hero-links');
-    const btn = document.querySelector('.ahti-hero-btn');
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        btn.classList.toggle('nav-active-right');
-    });
-}
+// const navSlide = () => {
+//     const burger = document.querySelector('.ahti-nav-burger');
+//     const nav = document.querySelector('.ahti-hero-links');
+//     const btn = document.querySelector('.ahti-hero-btn');
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+//         btn.classList.toggle('nav-active-right');
+//     });
+// }
 
 // let burger = document.getElementById('nav-links');
 // if(burger.style.display == "block"){
@@ -16,18 +16,24 @@ const navSlide = () => {
 //     burger.style.display="block";
 // }
 
-function myFunction() {
+
+function navSlider() {
+    var linksMedia =window.matchMedia("(max-width:768px)")
     var x = document.getElementById("nav-links");
-    if (x.style.visibility === "visible") {
-      x.style.visibility = "hidden";
+    if(linksMedia.matches)
+    {
+      if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+      }
+      else{
+        x.style.visibility = "visible";
+      }
+      if (x.style.opacity === "1") {
+        x.style.opacity = "0";
+      } else {
+        x.style.opacity = "1";
+      }
     } 
-    if (x.style.opacity === "1") {
-      x.style.opacity = "0";
-    } else {
-      x.style.opacity = "1";
-    }
   }
 
-
-navSlide();
 
