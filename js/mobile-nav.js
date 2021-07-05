8ad9090ea9d006bfbc2da4c9c3496a3f2e04481b
Ehtisham Ul Haq <ahtionpc1@gmail.com>
2021-07-05 16:24:31 +0500

    initial-commit

---
+++ js/mobile-nav.js
@@ -0,0 +1,35 @@
+const navSlide = () => {
+    const burger = document.querySelector('.ahti-nav-burger');
+    const nav = document.querySelector('.ahti-hero-links');
+    const btn = document.querySelector('.ahti-hero-btn');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        btn.classList.toggle('nav-active-right');
+    });
+}
+
+// let burger = document.getElementById('nav-links');
+// if(burger.style.display == "block"){
+//     burger.style.display="none";
+// }
+// else{
+//     burger.style.display="block";
+// }
+
+function myFunction() {
+    var x = document.getElementById("nav-links");
+    if (x.style.visibility === "visible") {
+      x.style.visibility = "hidden";
+    } else {
+      x.style.visibility = "visible";
+    }
+    if (x.style.opacity === "0") {
+      x.style.opacity = "1";
+    } else {
+      x.style.opacity = "1";
+    }
+  }
+
+
+navSlide();
+
