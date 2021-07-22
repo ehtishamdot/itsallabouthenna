c6eb7570e0a0600dc691b696fd00951de011fc78
Ehtisham Ul Haq <ahtionpc1@gmail.com>
2021-07-22 11:17:04 +0500

    service-hr-line-updation

---
+++ js/image-overlay.js
@@ -0,0 +1,7 @@
+let overlay = document.querySelector('.overlay');
+let imageOverlay =  document.querySelector('.image-overlay');
+let imageClick = document.querySelector(".gallery-container .img img");
+
+imageClick.addEventListener('click', function(){
+    
+})
